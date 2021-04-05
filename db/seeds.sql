2630c12cf422ab7db92c2a98257ac6589ecb178c
Caroline Tobin <ctobin25>
2021-04-05 18:44:01 -0400

    completed get route

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('cheese');
+INSERT INTO burgers (name) VALUES ('bacon');
+INSERT INTO burgers (name, eaten) VALUES ('vegan', true);
+INSERT INTO burgers (name, eaten) VALUES ('bean', true);
+INSERT INTO burgers (name, eaten) VALUES ('beef', true);
+INSERT INTO burgers (name) VALUES ('double');
